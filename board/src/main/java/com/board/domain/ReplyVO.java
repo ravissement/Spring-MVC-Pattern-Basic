cba0cac5645946acd70d51b1e212fe4931abe240
ravissement <ravissement55@gmail.com>
2021-04-06 15:43:10 +0900

    게시판 댓글 기능 추가

---
+++ board/src/main/java/com/board/domain/ReplyVO.java
@@ -0,0 +1,57 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class ReplyVO {
+	/*
+ 	CREATE TABLE tbl_reply (
+	rno	INT	NOT NULL AUTO_INCREMENT,
+	bno	INT	NOT NULL,
+	writer	VARCHAR(30)	NOT NULL,
+	content	TEXT			NOT NULL,
+	regDate	TIMESTAMP	NOT NULL DEFAULT NOW(),
+	PRIMARY KEY(rno, bno),
+	FOREIGN KEY(bno) REFERENCES tbl_board(bno)
+	); 
+	*/
+	
+	private int rno;
+	private int bno;
+	private String writer;
+	private String content;
+	private Date regDate;
+	
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+
+}
