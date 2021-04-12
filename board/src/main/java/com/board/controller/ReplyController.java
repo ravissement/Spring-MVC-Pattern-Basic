983628f797305cd31a98e3df23bc69fde77b8f6a
ravissement <ravissement55@gmail.com>
2021-04-12 09:13:20 +0900

    댓글 수정기능 추가

--- board/src/main/java/com/board/controller/ReplyController.java
+++ board/src/main/java/com/board/controller/ReplyController.java
@@ -5,7 +5,6 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.domain.ReplyVO;
 import com.board.service.ReplyService;
